27873345f367f26cf0c7e2f02d411cfc9f4b759b
1241273-hugo2006alm <1241273@isep.ipp.pt>
2026-03-20 23:44:20 +0000

    ref(database): Use DB URL and SSL options
    Update Lucid database configuration to use a DB_URL connection string with optional SSL settings. Update environment schema and example variables for DB_URL, DB_SSL, and DB_SSL_REJECT_UNAUTHORIZED.

--- start/env.ts
+++ start/env.ts
@@ -35,4 +35,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
+  DB_URL: Env.schema.string.optional(),
+  DB_SSL: Env.schema.boolean.optional(),
+  DB_SSL_REJECT_UNAUTHORIZED: Env.schema.boolean.optional(),
 })
